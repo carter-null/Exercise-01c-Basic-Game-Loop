f797bcdbe8c5c39f7ce0db5ca091fa7b6a597bcd
carter-null <70243434+carter-null@users.noreply.github.com>
2020-09-03 17:55:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def get_input(current):
 def main():
     game_desc = load("adventure.json")
     current = find_passage(game_desc, game_desc["startnode"])
+    print(current["name"])
     choice = ""
 
     while choice != "quit" and current != {}:
